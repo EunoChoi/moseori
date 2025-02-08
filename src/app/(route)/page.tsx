ed662ee80fc6a525a55a0fd0e3fc6450e4d0b220
euno <chonist@gmail.com>
2025-02-08 11:07:01 +0900

    chore: 폴더명 변경, (route) -> (routes)

--- src/app/(route)/page.tsx
+++
@@ -1,8 +0,0 @@
-
-/**메인 페이지 - 모집 공고*/
-const Page = () => {
-  return (<>
-  </>)
-}
-
-export default Page;
---
+++ src/app/(routes)/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import styled from "styled-components";
+
+//components
+import MainHeader from "@/common/components/MainHeader";
+
+/**메인 페이지 - 모집 공고*/
+const Page = () => {
+  return (<Wrapper>
+    <MainHeader />
+  </Wrapper>)
+}
+
+export default Page;
+
+const Wrapper = styled.div`
+
+`
