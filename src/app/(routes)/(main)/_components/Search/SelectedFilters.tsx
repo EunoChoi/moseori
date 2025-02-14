44f277babeaa9dff8647c7afd0830205f1528935
euno <chonist@gmail.com>
2025-02-15 08:18:35 +0900

    feat: SelectedFilters 컴포넌트, 스크롤시 나타나는 페이드 블럭 추가

--- src/app/(routes)/(main)/_components/Search/SelectedFilters.tsx
+++ src/app/(routes)/(main)/_components/Search/SelectedFilters.tsx
@@ -1,56 +1,122 @@
-import CloseIcon from '@mui/icons-material/Close';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import SettingsIcon from '@mui/icons-material/Settings';
+import { useInView } from 'react-intersection-observer';
 import styled from "styled-components";
 
-const SelectedFilters = ({ isVisible }: { isVisible?: boolean }) => {
-  const FILTER_TYPES = ['진행중', '자기계발'];
+const SelectedFilters = () => {
+  const FILTER_TYPES = ['모든 공고', '자기계발x 소설x 에세이x 역사x 종교x', '마감일'];
   // 필터 타입이 용이하도록 코드 작성 필요
   // 버튼 누른 후 마운트되는 필터 리스트에서 버튼에 해당하는 위치로 스크롤하기 위한 ref가 들어있으면 좋을듯
 
-  return (<Wrapper className={isVisible ? 'hide' : ''}>
-    <Empty />
-    {FILTER_TYPES.map((e) =>
-      <FilterTypeButton key={'filterType' + e}>
-        <span>{e}</span>
-        <CloseIcon className='icon' fontSize='inherit' color='inherit' />
-        <ExpandMoreIcon className='icon' fontSize='inherit' color='inherit' />
-      </FilterTypeButton>)}
+  //inView 2개 사용해서 첫번째가 조금이라도 안보이면 left, 두번째가 조금이라도 안보이면 right
+  const { ref: firstItemRef, inView: firstTargetInView } = useInView({
+    initialInView: true,
+    threshold: 0.99
+  });
+  const { ref: secondItemRef, inView: secondTargetInView } = useInView({
+    initialInView: true,
+    threshold: 0.99
+  });
 
-    <FilterTypeButton className='more'>
-      <SettingsIcon className='icon' fontSize='inherit' color='inherit' />
-    </FilterTypeButton>
-    <Empty />
+
+  return (<Wrapper>
+    {firstTargetInView === false && <Blur className='left' />}
+    {secondTargetInView === false && <Blur className='right' />}
+    <ScrollWrapper>
+      <Empty />
+      <FilterTypeButton ref={firstItemRef} className='more'>
+        <SettingsIcon className='icon' fontSize='inherit' color='inherit' />
+      </FilterTypeButton>
+      {FILTER_TYPES.map((e, i) =>
+        <FilterTypeButton key={'filterType' + e} ref={i === FILTER_TYPES.length - 1 ? secondItemRef : undefined}>
+          <span>{e}</span>
+          {/* <CloseIcon className='icon' fontSize='inherit' color='inherit' /> */}
+          <ExpandMoreIcon className='icon' fontSize='inherit' color='inherit' />
+        </FilterTypeButton>)}
+      <Empty />
+    </ScrollWrapper>
   </Wrapper>);
 }
 
 export default SelectedFilters;
 
-// 가운데 정렬을 위한 요소, 
-const Empty = styled.div`
-  width: 100%;
-`
-
 const Wrapper = styled.div`
-  display: flex;
-  justify-content: start;
-  align-items:center;
-  gap: 8px;
-  /* padding: 0 8px; */
-
-  overflow-x: scroll;
+  position: relative;
+  box-sizing: border-box;
+  overflow: hidden;
 
   @media (max-width: 479px) { //mobile port
-    width: 100dvw;
-    height: 28px;
+    width: 100dvw; 
+    height: 30px;
   }
-  @media (min-width:480px) and (max-width:1023px) { //mobild land + tablet
+  @media (min-width:480px) and (max-width:1023px) { //mobile land + tablet
     width: auto;
     height: 36px;
   }
   @media (min-width:1024px) { //desktop
     width: auto;
     height: 36px;
+  }
+`
+const ScrollWrapper = styled.div`
+  width: inherit;
+  height: inherit;
+  overflow-x: scroll;
+
+  display: flex;
+  justify-content: start;
+  align-items:center;
+  gap : 8px;
+
+  @media (max-width: 479px) { //mobile port
+    padding: 0 5dvw;
+  }
+  @media (min-width:480px) and (max-width:1023px) { //mobile land + tablet
+    margin-right: 8px;
+  }
+  @media (min-width:1024px) { //desktop
+    margin-right: 8px;
+  }
+`
+const Blur = styled.div`
+  animation: fadeIn ease-in-out 200ms;
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  flex-shrink: 0;
+  position: absolute;
+  top: -5px;
+
+  height: 150%;
+  width: 20px;
+
+  pointer-events: none;
+
+  @media (max-width: 479px) { //mobile port
+    &.left{
+    background: linear-gradient(to right, rgba(248, 249, 250, 1), rgba(248, 249, 250,0)); /* 왼쪽 페이드 */
+    left: -2px;
+    }
+    &.right{
+      right: -2px;
+      background: linear-gradient(to left, rgba(248, 249, 250, 1), rgba(248, 249, 250, 0) 100%);
+    }
+  }
+  @media (min-width:480px) { //mobild land + tablet
+    &.left{
+      background: linear-gradient(to right, rgba(248, 249, 250, 1), rgba(248, 249, 250,0)); /* 왼쪽 페이드 */
+      left: 0px;
+    }
+    &.right{
+      right: 8px;
+      background: linear-gradient(to left, rgba(248, 249, 250, 1), rgba(248, 249, 250, 0) 100%);
+    }
   }
 `
 
@@ -67,11 +133,15 @@ const FilterTypeButton = styled.button`
   gap: 8px;
 
   background-color: var(--main-color);
+  background-color: var(--sub-color);
+
+  span, .icon{
+    color: var(--grey0);
+  }
 
   span{
     font-size: 14px;
     font-weight: 500;
-    color: white;
 
     display: flex;
     justify-content: center;
@@ -79,7 +149,6 @@ const FilterTypeButton = styled.button`
   }
   .icon{
     font-size: 16px;
-    color: white;
   }
 
   &.more{
@@ -89,3 +158,13 @@ const FilterTypeButton = styled.button`
   }
 `
 
+// 가운데 정렬을 위한 요소, 
+const Empty = styled.div`
+  width: 100%;
+  &:first-child{
+    margin-left: -8px;
+  }
+  &:last-child{
+    margin-left: -8px;
+  }
+`
