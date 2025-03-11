d40f88db1ab762e1f7081c14aff37e4b53d6dabc
euno <chonist@gmail.com>
2025-03-12 03:12:20 +0900

    feat: search 필요 요소 url query parameter 연동 훅스 생성

--- src/app/(routes)/(main)/_components/Search/hooks/useSearchSync.ts
+++ src/app/(routes)/(main)/_components/Search/hooks/useSearchSync.ts
@@ -1,17 +1,42 @@
 import { useSearchContext } from "@/common/store/useSearchContext";
+import { usePathname, useRouter } from "next/navigation";
 import { useEffect } from "react";
+
+interface Props {
+  pathname: string;
+  sortType: number[],
+  categoryType: number[],
+  searchKeyword: string;
+}
+
+const makeUrl = ({ pathname, sortType, categoryType, searchKeyword }: Props) => {
+  const params = new URLSearchParams();
+
+  if (searchKeyword) {
+    params.set("search", searchKeyword);
+  }
+  if (sortType && sortType.length > 0) {
+    params.set("sort", sortType[0].toString());
+  }
+  if (categoryType && categoryType.length > 0) {
+    categoryType.forEach(cat => params.append("cat", cat.toString()));
+  }
+
+  const queryString = params.toString();
+  return queryString ? `${pathname}?${queryString}` : pathname;
+}
 
 /** url 쿼리 파라미터 동기화 - search filter, search input state */
 const useSearchSync = () => {
 
+  const router = useRouter();
+  const pathname = usePathname();
   const { sortType, categoryType, searchKeyword } = useSearchContext();
 
   useEffect(() => {
-    //sortType, categoryType, searchKeyword 조합 url 만들고 푸시
-    // console.log(sortType, categoryType, searchKeyword)
+    const url = makeUrl({ pathname, sortType, categoryType, searchKeyword });
+    router.replace(url, { scroll: false })
   }, [sortType, categoryType, searchKeyword]);
-
-  return null;
 }
 
 export default useSearchSync;
