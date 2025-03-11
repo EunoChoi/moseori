b48f0c8d4822633c8b3ab7ef7a540989b4d0f4c8
euno <chonist@gmail.com>
2025-03-12 03:19:36 +0900

    fix: SearchProvider state 초기값 url query parameter로 부터 얻어오도록 설정

--- src/common/store/SearchProvider.tsx
+++ src/common/store/SearchProvider.tsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from "next/navigation";
 import { createContext, ReactNode, useState } from "react";
 
 interface Option {
@@ -18,11 +19,17 @@ export const SearchContext = createContext<SearchContextProps | undefined>(undef
 
 /** 검색시 필요한 search input text, search filter 제공 context */
 export const SearchProvider = ({ children }: { children: ReactNode }) => {
+  const readOnlyParams = useSearchParams();
+
+  const sortTypeByQueryParams = Number(readOnlyParams.get('sort'));
+  const catTypeByQueryParams = readOnlyParams.getAll('cat').map(Number);
+  const searchKeywordByQueryParams = readOnlyParams.get('search');
+
 
   //새로고침 후 값 유지하기 위해 쿼리 파라미터로부터 초기값을 가져온다.
-  const [searchKeyword, setSearchKeyword] = useState<string>("");
-  const [sortType, setSortType] = useState<number[]>([0]);
-  const [categoryType, setCategoryType] = useState<number[]>([]);
+  const [searchKeyword, setSearchKeyword] = useState<string>(searchKeywordByQueryParams ? searchKeywordByQueryParams : "");
+  const [sortType, setSortType] = useState<number[]>(sortTypeByQueryParams ? [sortTypeByQueryParams] : [0]);
+  const [categoryType, setCategoryType] = useState<number[]>(catTypeByQueryParams ? catTypeByQueryParams : []);
 
   return (<SearchContext.Provider
     value={{
